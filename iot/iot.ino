70de5ad554580335a0be01162af2c687975b956b
munazr <munaz_Rahman@hotmail.com>
2016-06-25 15:14:53 -0400

    Add servo

--- iot/iot.ino
+++ iot/iot.ino
@@ -1,13 +1,33 @@
+#include <Servo.h>
+
 int led = 13;
+
+Servo servo;
+int servoPos = 0;
 
 void setup() {  
   // Initialize LED output
-  pinMode(led, OUTPUT);    
+  pinMode(led, OUTPUT);
+  
+  servo.attach(9);
 }
 
 void loop() {
-  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);               // wait for a second
+  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level) 
+ 
+  for (servoPos = 0; servoPos < 90; servoPos += 1)
+  {
+    servo.write(servoPos);
+    delay(10);  
+  }
+  
   digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);               // wait for a second  
+  
+  delay(2500);
+ 
+  for (servoPos = 90; servoPos >= 1; servoPos -= 1)
+  {
+    servo.write(servoPos);
+    delay(10);
+  }
 }
