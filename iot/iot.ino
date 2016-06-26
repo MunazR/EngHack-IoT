c3b39ab48ea5898205b4f731ab7af2919dde02e8
munazr <munaz_Rahman@hotmail.com>
2016-06-25 20:41:28 -0400

    Add piezoelectric speaker

--- iot/iot.ino
+++ iot/iot.ino
@@ -3,9 +3,10 @@
 int LED_PIN = 13;
 int SERVO_PIN = 9;
 int BUTTON_PIN = 8;
+int SPEAKER_PIN = 6;
 
 Servo servo;
-int servo_pos = 0;
+int servo_pos = 90;
 int servo_state = 0;
 int servo_delay = 0;
 
@@ -14,12 +15,19 @@ void setup() {
 
   // Initialize LED output
   pinMode(LED_PIN, OUTPUT);
-  
+
   // Initialize button input
   pinMode(BUTTON_PIN, INPUT);
 
   // Initialize servo
   servo.attach(SERVO_PIN);
+
+  // Initialize speaker
+  pinMode(SPEAKER_PIN, OUTPUT);
+  
+  beep(50);
+  beep(50);
+  beep(50);
 
   Serial.println("Setup complete");
 }
@@ -32,14 +40,14 @@ void loop() {
 
 void updateServo() {
   if (servo_state == 0) {
-    if (servo_pos <= 90) {
+    if (servo_pos < 90) {
       servo_pos += 1;
       servo.write(servo_pos);
     }
   }
   else if (servo_state == 1) {
-    if (servo_pos <= 90) {
-      servo_pos += 1;
+    if (servo_pos > 0) {
+      servo_pos -= 1;
       servo.write(servo_pos);
     } 
     else {
@@ -48,7 +56,7 @@ void updateServo() {
     }
   }
   else if (servo_state == 2) {
-    if (servo_delay >= 200) {
+    if (servo_delay > 200) {
       servo_state = 3;
     } 
     else {
@@ -56,12 +64,21 @@ void updateServo() {
     }
   }
   else if (servo_state == 3) {
-    if (servo_pos >= 0) {
-      servo_pos -= 1;
+    if (servo_pos < 90) {
+      servo_pos += 1;
       servo.write(servo_pos);
     } 
     else {
+      servo_state = 4;
+      servo_delay = 0;
+    }
+  } 
+  else if (servo_state == 4) {
+    if (servo_delay > 200) {
       servo_state = 1;
+    } 
+    else {
+      servo_delay += 1;
     }
   }
 }
@@ -73,19 +90,27 @@ void pollButton() {
     if (servo_state != 0) {
       Serial.println("Servo disabled");
       digitalWrite(LED_PIN, LOW);
+      beep(50);
       servo_state = 0;
     } 
     else {
       Serial.println("Servo enabled");
       digitalWrite(LED_PIN, HIGH);
+      beep(50);
       servo_state = 1;
     }
 
-    while (digitalRead(BUTTON_PIN) == HIGH) { 
-      // Do nothing
-    }
+    while (digitalRead(BUTTON_PIN) == HIGH);
   }
 }
+
+void beep(unsigned char delayms) {
+  analogWrite(SPEAKER_PIN, 20);
+  delay(delayms);
+  analogWrite(SPEAKER_PIN, 0);
+  delay(delayms);
+}
+
 
 
 
