2331a6ebb2132d61fc6344b03b9f682a8d65fbff
munazr <munaz_Rahman@hotmail.com>
2016-06-26 00:03:16 -0400

    Add LED functionality

--- iot/iot.ino
+++ iot/iot.ino
@@ -1,9 +1,12 @@
 #include <Servo.h>
 
-int LED_PIN = 13;
-int SERVO_PIN = 9;
-int BUTTON_PIN = 8;
-int SPEAKER_PIN = 6;
+const int LED_PIN = 13;
+const int SERVO_PIN = 9;
+const int BUTTON_PIN = 8;
+const int SPEAKER_PIN = 6;
+const int RED_LED_PIN = 12;
+const int GREEN_LED_PIN = 11;
+const int BLUE_LED_PIN = 10;
 
 Servo servo;
 int servo_pos = 90;
@@ -13,8 +16,11 @@ int servo_delay = 0;
 void setup() {
   Serial.begin(9600);
 
-  // Initialize LED output
+  // Initialize LED outputs
   pinMode(LED_PIN, OUTPUT);
+  pinMode(RED_LED_PIN, OUTPUT);
+  pinMode(GREEN_LED_PIN, OUTPUT);
+  pinMode(BLUE_LED_PIN, OUTPUT);
 
   // Initialize button input
   pinMode(BUTTON_PIN, INPUT);
@@ -25,8 +31,12 @@ void setup() {
   // Initialize speaker
   pinMode(SPEAKER_PIN, OUTPUT);
   
+  
+  rgbLED(255, 0, 0);
   beep(50);
+  rgbLED(0, 255, 0);
   beep(50);
+  rgbLED(0, 0, 255);
   beep(50);
 
   Serial.println("Setup complete");
@@ -90,12 +100,14 @@ void pollButton() {
     if (servo_state != 0) {
       Serial.println("Servo disabled");
       digitalWrite(LED_PIN, LOW);
+      rgbLED(255, 0, 0);
       beep(50);
       servo_state = 0;
     } 
     else {
       Serial.println("Servo enabled");
       digitalWrite(LED_PIN, HIGH);
+      rgbLED(0, 255, 0);
       beep(50);
       servo_state = 1;
     }
@@ -104,16 +116,16 @@ void pollButton() {
   }
 }
 
+void rgbLED(int red, int green, int blue) {
+  analogWrite(RED_LED_PIN, red);
+  analogWrite(GREEN_LED_PIN, green);
+  analogWrite(BLUE_LED_PIN, blue);  
+}
+
 void beep(unsigned char delayms) {
   analogWrite(SPEAKER_PIN, 20);
   delay(delayms);
   analogWrite(SPEAKER_PIN, 0);
   delay(delayms);
 }
-
-
-
-
-
-
 
